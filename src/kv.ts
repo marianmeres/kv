be23fe4ed71367abc1277e4df19412a599d5efcb
Marian Meres <marian@meres.sk>
2025-10-31 21:24:00 +0100

    type fix

--- src/kv.ts
+++ src/kv.ts
@@ -24,7 +24,7 @@ export function createKVClient<T extends keyof KnownTypes>(
 	if (type === "postgres") return new AdapterPostgres(namespace, options);
 	if (type === "redis") return new AdapterRedis(namespace, options);
 	if (type === "deno-kv") {
-		if (typeof Deno === "undefined") {
+		if (typeof globalThis.Deno === "undefined") {
 			throw new Error(`Type "${type}" is only supported under Deno runtime`);
 		}
 		return new AdapterDenoKv(namespace, options);
