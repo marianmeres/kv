8529b47f4028e214a1d4212f14514a9026c90ed9
Marian Meres <marian@meres.sk>
2026-04-18 17:52:31 +0200

    deps

--- scripts/build-npm.ts
+++ scripts/build-npm.ts
@@ -1,4 +1,4 @@
-import { npmBuild } from "@marianmeres/npmbuild";
+import { npmBuild, versionizeDeps } from "@marianmeres/npmbuild";
 
 const denoJson = JSON.parse(Deno.readTextFileSync("deno.json"));
 
@@ -6,11 +6,14 @@ await npmBuild({
 	name: denoJson.name,
 	version: denoJson.version,
 	repository: denoJson.name.replace(/^@/, ""),
-	dependencies: [
-		"@marianmeres/clog",
-		"pg",
-		"redis",
-		"@types/pg",
-		"@types/redis",
-	],
+	dependencies: versionizeDeps(
+		[
+			"@marianmeres/clog",
+			"pg",
+			"redis",
+			"@types/pg",
+			"@types/redis",
+		],
+		"../deno.json",
+	),
 });
